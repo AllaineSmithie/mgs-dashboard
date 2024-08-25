698084defeac3b06de37908152280ca091db04ca
AllaineSmithie <deadlinemusicservices@gmail.com>
2024-08-25 22:17:20 +0200

    ...

---
+++ src/pages/pages/index.tsx
@@ -0,0 +1,27 @@
+/*************************************************************************/
+/*  index.tsx                                                            */
+/*************************************************************************/
+/* Copyright Deadline Entertainment Gbr                                  */
+/* SPDX-License-Identifier: AGPL-3.0-only                                */
+
+import MainLayout from '@components/MainLayout'
+
+export default function Pages() {
+  return (
+    <MainLayout
+      breadcrumb={{
+        breadcrumbCurrentText: 'Messages',
+      }}
+    >
+      <PagesList />
+    </MainLayout>
+  )
+}
+
+
+function PagesList() {
+    return (
+      <div>
+      </div>
+    )
+}
